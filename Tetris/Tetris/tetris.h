5e6ee95bc147202dce0e2c6fa454d097fea0d9df
KDY <kdy542710@gmail.com>
2022-09-18 10:24:16 +0900

    tetris - 파일 분할

---
+++ Tetris/Tetris/tetris.h
@@ -0,0 +1,18 @@
+#ifndef TETRIS_H
+#define TETRIS_H
+
+void CursorView();
+void gotoxy(int x, int y);
+void init_game(void);
+void draw_board();
+void draw_block();
+void delete_pre_block();
+int can_move(int x, int y);
+void drop_block();
+void cumulate_block();
+void input_command();
+void remove_line_and_get_score();
+int is_finished();
+void print_menu();
+
+#endif
